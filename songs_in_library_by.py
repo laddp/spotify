97a656d878815a04efc69eb84cd49e60d9f004b7
Patrick Ladd <pladd@redhat.com>
2019-04-17 23:34:24 -0400

    Better file argument handling

--- songs_in_library_by.py
+++ songs_in_library_by.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("artist",
                     help="The artist to search for")
 parser.add_argument("library_file",
-                    help="The JSON library file to search")
+                    help="The JSON library file to search", type=argparse.FileType('r'))
 parser.add_argument("sort_field",
                     help="Field to sort results on (default:name)", nargs="?", default="name",
                     choices=['name','popularity','release_date','duration_ms'])
@@ -15,8 +15,7 @@ parser.add_argument("-r", "--reverse",
                     help="Reverse sort order", action="store_true")
 args = parser.parse_args()
 
-libfile = open(args.library_file)
-library = json.load(libfile)
+library = json.load(args.library_file)
 
 entries = filter(lambda entry: entry['track']
                  ['artists'][0]['name'] == args.artist, library)
